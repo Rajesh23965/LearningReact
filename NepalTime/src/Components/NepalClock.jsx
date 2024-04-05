37b021b94344ce21f8a77d65c0bdc2cf1344b3f2
Rajesh23965 <rajeshky123456@gmail.com>
2024-04-05 11:14:12 +0545

    css is added in nepaltime

--- NepalTime/src/Components/NepalClock.jsx
+++ NepalTime/src/Components/NepalClock.jsx
@@ -1,6 +1,8 @@
+import React from "react";
+import styles from "./style.module.css";
 export default function NepalClock() {
   return (
-    <div style={{ textAlign: "center" }}>
+    <div>
       <h1 className="lead">Nepal Time</h1>
       <p className="lead">
         This is the clock that shows the current Date and time in Nepal at all
